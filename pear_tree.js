239ca81b253943960d30452f11c2a88b722a9a81
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-03-13 16:18:41 +0700

    tugas kebun rama

---
+++ pear_tree.js
@@ -0,0 +1,127 @@
+"use strict"
+
+class FruitTree {
+  constructor(){
+    this._healthyStatus = true;
+    this._harvested = null;
+    this._fruits = 10;
+    this._goodQuality = 0;
+    this._badQuality = 0;
+    this._age = 0;
+    this._height =0;
+    this.deadAge = 15;
+    this.matureAge = 5
+    this.growthHeight = 15;
+  }
+
+  get age() {
+    return this._age
+  }
+  get height() {
+    return this._height.toFixed(1)
+  }
+  get fruits() {
+    return this._fruits
+  }
+
+  get healthyStatus() {
+    return this._healthyStatus
+  }
+
+  get harvested() {
+    return this._harvested;
+  }
+
+  get goodQuality(){
+    return this._goodQuality;
+  }
+
+  get badQuality(){
+    return this._badQuality;
+  }
+
+  grow() {
+    this._age = this._age + 1;
+    if(this._age<=this.matureAge){
+      let growHeight = Math.floor(Math.random()*Math.floor(this.growthHeight))+1;
+      this._height = this._height + growHeight/10;
+    }
+    else{
+      this._height = this._height;
+    }
+    if(this._age>this.deadAge){
+      this._healthyStatus = false;
+    }
+  }
+
+  produceFruits() {
+
+    let totalFruits = [];
+
+    if(this._age>=this.matureAge){
+      let totalProduce = this._fruits - Math.floor(Math.random()*Math.floor(8))+1;
+      for(let i=totalProduce;i>0;i--){
+        let qualityRandom = Math.random();
+        if(qualityRandom>=0.5){
+          let quality = 'good';
+          let fruits = new Fruit(quality);
+          totalFruits.push(fruits);
+        }
+        else{
+          let quality = 'bad';
+          let fruits = new Fruit(quality);
+          totalFruits.push(fruits);
+        }
+      }
+    }
+    return totalFruits;
+  }
+
+  harvest() {
+    let totalFruits = this.produceFruits();
+    this._harvested = totalFruits.length;
+    this._goodQuality =0;
+    this._badQuality =0;
+    for(let i=0;i<totalFruits.length;i++){
+      if(totalFruits[i].quality == 'good'){
+        this._goodQuality++;
+      }
+      else{
+        this._badQuality++
+      }
+    }
+  }
+}
+class Fruit {
+  constructor(quality) {
+    this.quality = quality;
+  }
+}
+
+class PearTree extends FruitTree{
+  constructor(age,matureAge,height,healthyStatus){
+    super();
+
+    this._age = age;
+    this.matureAge = matureAge;
+    this._height = height;
+    this._healthyStatus = healthyStatus;
+  }
+}
+class Pear extends Fruit{
+  constructor(quality){
+    this.quality = quality;
+  }
+}
+
+
+let pearTree = new PearTree(0,10,3,true)//"nama,initialAge,matureAge,height,healthyStatus"
+  // console.log('---------------------------Apple Tree------------------------------');
+do {
+  pearTree.grow();
+  pearTree.produceFruits();
+  pearTree.harvest();
+//  console.log(`[Year ${pearTree.age} Report] Height = ${pearTree.height} | Fruits harvested = ${pearTree.harvested} (${pearTree.goodQuality} good, ${pearTree.badQuality} bad)`)
+} while (pearTree.healthyStatus != false)
+
+module.exports = {PearTree, Pear}
--- tree_grove.js
+++ tree_grove.js
@@ -1,8 +1,72 @@
-const MangoTree = require('./mango_tree.js');
-const AppleTree = require('./apple_tree.js');
-const PearTree = require('./pear_tree.js');
+const MangoTreeImport = require('./mango_tree.js');
+const AppleTreeImport = require('./apple_tree.js');
+const PearTreeImport = require('./pear_tree.js');
 
-class TreeGrove {}
+const MangoTree = MangoTreeImport.MangoTree;
+const AppleTree = AppleTreeImport.AppleTree;
+const PearTree = PearTreeImport.PearTree
+
+class TreeGrove {
+  constructor(){
+    this.farm = []
+  }
+
+  inputTree(name,initialAge,matureAge,height,healthyStatus){
+    this.growGroove = []
+    var tree;
+    if(name == "MangoTree"){
+      tree = new MangoTree(initialAge,height,matureAge,healthyStatus);
+    }
+    else if (name == "AppleTree") {
+      tree = new AppleTree(initialAge,height,matureAge,healthyStatus);
+    }
+    else if(name =="PearTree"){
+      tree = new PearTree(initialAge,height,matureAge,healthyStatus);
+    }
+    tree.name = name;
+    this.farm.push(tree)
+  }
+  nextYear(){
+    for(let i=0;i<this.farm.length;i++){
+      this.farm[i].grow();
+    }
+
+  }
+  showTrees(){
+    this.treesName = []
+
+    for(let i=0;i<this.farm.length;i++){
+        this.treesName.push(this.farm[i].name);
+    }
+    console.log('---Name Trees---',this.treesName);
+  }
+  showAge(){
+    this.treesAge = []
+
+    for(let i=0;i<this.farm.length;i++){
+        this.treesAge.push(this.farm[i]._age);
+    }
+    console.log('---Age Trees---',this.treesAge)
+  }
+  mature_trees(){
+    this.matureTrees = [];
+    for(let i=0;i<this.farm.length;i++){
+      if(this.farm[i]._age<this.farm[i].matureAge){
+        this.matureTrees.push(this.farm[i].name);
+      }
+    }
+    console.log('---Mature Trees---',this.matureTrees);
+  }
+  dead_trees(){
+    this.deadTrees = [];
+    for(let i=0;i<this.farm.length;i++){
+      if(this.farm[i]._age>this.farm[i].matureAge){
+        this.deadTrees.push(this.farm[i].name);
+      }
+    }
+    console.log('---Dead Trees---',this.deadTrees);
+  }
+}
 
 var grove = new TreeGrove()
 // input your trees data !
@@ -15,6 +79,17 @@ grove.inputTree("MangoTree", 3, 1.8, 7,true)
 grove.inputTree("MangoTree", 5, 2.4, 12,true)
 grove.inputTree("AppleTree", 4, 1.2, 5,true)
 grove.inputTree("PearTree", 7, 2, 15,true)
+
+
+
+for(let i=1;i<6;i++){
+  console.log('------------------ HASIL TAHUN : ' + i + ' -------------------' +'\n');
+  grove.nextYear();
+  for(let j=0;j<grove.farm.length;j++){
+    console.log(`${grove.farm[j].name} ${grove.farm[j]._age} tahun Height: ${grove.farm[j]._height.toFixed(1)}`);
+  }
+  console.log('\n');
+}
 
 // next year
 grove.nextYear()
