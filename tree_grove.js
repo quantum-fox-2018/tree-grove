486662964a7d360d31115c81c3d0532f2c190dde
Rafie Gilang <gilang0293@gmail.com>
2018-03-13 12:11:56 +0700

    sudah selesai tree grove

--- tree_grove.js
+++ tree_grove.js
@@ -1,8 +1,64 @@
-const MangoTree = require('./mango_tree.js');
-const AppleTree = require('./apple_tree.js');
-const PearTree = require('./pear_tree.js');
+const Tree = require('./mango_tree.js');
+//const AppleTree = require('./apple_tree.js');
+//const PearTree = require('./pear_tree.js');
 
-class TreeGrove {}
+class TreeGrove {
+  constructor(){
+    this.tree = [];
+  }
+
+  inputTree(name, age, height, matureAge, healthyStatus){
+    switch(name){
+      case 'MangoTree':
+        this.tree.push([name,Tree.MangoTree(age, height, matureAge, healthyStatus)]);
+        break;
+      case 'AppleTree':
+        this.tree.push([name,Tree.AppleTree(age, height, matureAge, healthyStatus)]);
+        break;
+      case 'PearTree':
+        this.tree.push([name,Tree.PearTree(age, height, matureAge, healthyStatus)]);
+        break;
+    }
+  }
+
+  nextYear(){
+    for(let index in this.tree){
+      let death_roll = Math.floor(Math.random() * (10 - 0) + 0);
+      this.tree[index][1]._age++;
+      if(this.tree[index][1].age < this.tree[index][1].matureAge){
+        this.tree[index][1]._height += Math.random();
+        this.tree[index][1]._height.toFixed(1);
+      }
+      if(death_roll === 9){
+        this.tree[index][1]._healthyStatus = false;
+      }
+    }
+  }
+
+  showAge(){
+    for(let index in this.tree)console.log(this.tree[index][0] +' in pot ' + (index) + ' is '+this.tree[index][1].age + ' Years old');
+    console.log('\n');
+  }
+
+  showTrees(){
+    for(let index in this.tree)console.log('the tree in pot ' + (index) + ' is a '+ this.tree[index][0]);
+    console.log('\n');
+  }
+
+  mature_trees(){
+    for(let index in this.tree){
+      if(this.tree[index][1].age >= this.tree[index][1].matureAge) console.log(this.tree[index][0] + ' in pot ' + (index) + ' is already matured');
+    }
+    console.log('\n');
+  }
+
+  dead_trees(){
+    for(let index in this.tree){
+      if(this.tree[index][1].healthyStatus === false) console.log(this.tree[index][0] + ' in pot ' + (index) + ' is unfortunately dead');
+    }
+    console.log('\n');
+  }
+}
 
 var grove = new TreeGrove()
 // input your trees data !
@@ -16,17 +72,21 @@ grove.inputTree("MangoTree", 5, 2.4, 12,true)
 grove.inputTree("AppleTree", 4, 1.2, 5,true)
 grove.inputTree("PearTree", 7, 2, 15,true)
 
-// next year
-grove.nextYear()
+for(let year = 0; year < 10; year++){
+  console.log('++++++++ Year ' + (year+1) + ' report ++++++++++\n');
 
-// show trees age
-grove.showAge()
+  // next year
+  grove.nextYear()
 
-// show trees
-grove.showTrees()
+  // show trees
+  grove.showTrees()
 
-// show trees
-grove.mature_trees()
+  // show trees age
+  grove.showAge()
 
-// show trees
-grove.dead_trees()
+  // show trees
+  grove.mature_trees()
+
+  // show trees
+  grove.dead_trees()
+}
