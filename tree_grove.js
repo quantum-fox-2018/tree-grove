20302da037966b0d39490d6e84cf7f62b276288b
asepridwan1989 <aseprmail@gmail.com>
2018-03-13 19:03:33 +0700

    upload tree-grove

--- tree_grove.js
+++ tree_grove.js
@@ -2,7 +2,71 @@ const MangoTree = require('./mango_tree.js');
 const AppleTree = require('./apple_tree.js');
 const PearTree = require('./pear_tree.js');
 
-class TreeGrove {}
+class TreeGrove {
+  constructor() {
+      this.trees = []
+  }
+
+  inputTree(treeName, firstAge, firstHeight, matureAge, healtyStatus){
+    var inputPohon = ''
+      if(treeName == 'MangoTree'){
+          inputPohon = new MangoTree(treeName, firstAge, firstHeight, matureAge, healtyStatus)
+      }else if(treeName == 'AppleTree'){
+          inputPohon = new AppleTree(treeName, firstAge, firstHeight, matureAge, healtyStatus)
+      }else if(treeName == 'PearTree'){
+          inputPohon = new PearTree(treeName, firstAge, firstHeight, matureAge, healtyStatus)
+      }
+    this.trees.push(inputPohon)
+  }
+
+  show_ages(){
+    let len = this.trees.length
+    let umur = []
+    for(let i = 0; i < len; i++){
+        umur.push(this.trees[i].age)
+    }
+    return umur
+  }
+
+  show_trees(){
+    let len = this.trees.length
+    let  trees= []
+    for(let i = 0; i < len; i++){
+        trees.push(this.trees[i].treeName)
+    }
+    return trees
+  }
+
+  next_year(){
+    let len = this.trees.length
+    for(let i = 0; i < len; i++){
+      this.trees[i].grow()
+    }
+  }
+
+  mature_trees(){
+    let mature = []
+    let len = this.trees.length
+
+    for(let i = 0; i < len; i++){
+      if(this.trees[i].age >= this.trees[i].matureAge && this.trees[i].age < this.trees[i].maxAge){
+        mature.push(this.trees[i].treeName)
+      }
+    }
+    return mature
+  }
+
+  dead_trees(){
+    let dead = []
+    let len = this.trees.length
+    for(let i = 0; i < len; i++){
+      if(this.trees[i].age === this.trees[i].maxAge){
+        dead.push(this.trees[i].treeName)
+      }
+    }
+    return dead
+  }
+}
 
 var grove = new TreeGrove()
 // input your trees data !
@@ -15,18 +79,11 @@ grove.inputTree("MangoTree", 3, 1.8, 7,true)
 grove.inputTree("MangoTree", 5, 2.4, 12,true)
 grove.inputTree("AppleTree", 4, 1.2, 5,true)
 grove.inputTree("PearTree", 7, 2, 15,true)
-
-// next year
-grove.nextYear()
-
-// show trees age
-grove.showAge()
-
-// show trees
-grove.showTrees()
-
-// show trees
-grove.mature_trees()
-
-// show trees
-grove.dead_trees()
+console.log(grove)
+for(let i =1 ; i<10; i++){
+  grove.next_year()
+}
+console.log('umur pohon = ',grove.show_ages())
+console.log('daftar pohon = ',grove.show_trees())
+console.log('daftar pohon berbuah = ',grove.mature_trees())
+console.log('daftar pohon mati = ',grove.dead_trees())
